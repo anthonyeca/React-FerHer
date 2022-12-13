cf5a0b44321837ba9cd325d01c2a748c2de6867c
anthonyeca <anthonyeca19esp@gmail.com>
2022-12-13 21:37:46 +0100

    Message.jsx created

--- 05-hook-app/src/02-useEffect/SimpleForm.jsx
+++ 05-hook-app/src/02-useEffect/SimpleForm.jsx
@@ -1,4 +1,33 @@
+import { useState, useEffect } from 'react'
+import { Message } from './Message'
+
 export const SimpleForm = () => {
+  const [formState, setFormState] = useState({
+    username: 'strider',
+    email: 'fernando@gmail.com',
+  })
+
+  const { username, email } = formState
+
+  const onInputChange = ({ target }) => {
+    const { name, value } = target
+    setFormState({
+      ...formState,
+      [name]: value,
+    })
+  }
+
+  useEffect(() => {
+    // console.log('SE LLAMO EL USEEFFECT')
+  }, [])
+
+  useEffect(() => {
+    // console.log('formState CAMBIO')
+  }, [formState])
+
+  useEffect(() => {
+    // console.log('email CAMBIO')
+  }, [email])
   return (
     <>
       <h1>Formulario Simple</h1>
@@ -8,14 +37,19 @@ export const SimpleForm = () => {
         className="form-control"
         placeholder="Username"
         name="username"
+        value={username}
+        onChange={onInputChange}
       />
 
       <input
         type="email"
-        className="form-control"
+        className="form-control mt-2"
         placeholder="Fernando@google.com"
         name="email"
+        value={email}
+        onChange={onInputChange}
       />
+      {username === 'strider2' && <Message />}
     </>
   )
 }
