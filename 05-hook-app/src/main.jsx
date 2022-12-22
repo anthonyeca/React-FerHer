df06767ae8a1b82711b3a95f59857a3ca147aad4
anthonyeca <anthonyeca19esp@gmail.com>
2022-12-22 12:33:32 +0100

    More examples and useRef

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -4,12 +4,14 @@ import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 // import { HooksApp } from './HooksApp'
 // import { CounterApp } from './01-useState/CounterApp'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
-import { SimpleForm } from './02-useEffect/SImpleForm'
+// import { SimpleForm } from './02-useEffect/SImpleForm'
+// import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+import { FocusScreen } from './04-useRef/FocusScreen'
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-  <FormWithCustomHook />
+  <FocusScreen />
   // </React.StrictMode>
 )
