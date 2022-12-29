a8c4ad68a515f8f8bd3c450bdd38fc7832084003
anthony <anthonyeca.dev@gmail.com>
2022-12-29 22:38:16 +0100

    useLayout and memo

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -6,12 +6,14 @@ import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 // import { SimpleForm } from './02-useEffect/SImpleForm'
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
-import { FocusScreen } from './04-useRef/FocusScreen'
+// import { FocusScreen } from './04-useRef/FocusScreen'
+// import { Layout } from './05-useLayoutEffect/Layout'
+import { Memorize } from './06-memos/Memorize'
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-  <FocusScreen />
+  <Memorize />
   // </React.StrictMode>
 )
