bd62d99ca2c86211021307471539268299826ad8
anthony <anthonyeca.dev@gmail.com>
2023-01-02 16:49:50 +0100

    MemoHook & CallbackHook

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
+
 // import { HooksApp } from './HooksApp'
 // import { CounterApp } from './01-useState/CounterApp'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 // import { SimpleForm } from './02-useEffect/SImpleForm'
+// import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 // import { FocusScreen } from './04-useRef/FocusScreen'
 // import { Layout } from './05-useLayoutEffect/Layout'
-import { Memorize } from './06-memos/Memorize'
+// import { Memorize } from './06-memos/Memorize'
+// import { MemoHook } from './06-memos/MemoHook'
+import { CallbackHook } from './06-memos/CallbackHook'
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-  <Memorize />
+  <CallbackHook />
   // </React.StrictMode>
 )
