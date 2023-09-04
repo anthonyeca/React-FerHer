4720c441c9ec40e8949f005230169aa4cd6e8294
anthony <anthonyeca.dev@gmail.com>
2023-09-04 02:08:49 +0200

    05-hook-app useReducer

---
+++ 05-hook-app/src/08-useReducer/TodoApp.jsx
@@ -0,0 +1,31 @@
+import { Todolist } from './Todolist'
+import { TodoAdd } from './TodoAdd'
+import { useTodo } from '../hooks/useTodo'
+
+export const TodoApp = () => {
+  const { todos, handleDeleteTodo, handleToggleTodo, handleNewTodo } = useTodo()
+
+  return (
+    <>
+      <h1>
+        TodoApp: 10, <small>pendientes: 2</small>
+      </h1>
+      <hr />
+
+      <div className="row">
+        <div className="col-7">
+          <Todolist
+            todos={todos}
+            onDeleteTodo={handleDeleteTodo}
+            onToggleTodo={handleToggleTodo}
+          />
+        </div>
+        <div className="col-5">
+          <h4>Agregar TODO</h4>
+          <hr />
+          <TodoAdd onNewTodo={handleNewTodo} />
+        </div>
+      </div>
+    </>
+  )
+}
